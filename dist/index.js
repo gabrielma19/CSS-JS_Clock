6d4ddae9df2e96e7a84d5227a6832d2431e1bcdb
Gabriel Marcos <gabrielmareia@hotmail.com>
2017-03-13 19:32:03 -0300

    Clock made with CSS + JS + PUG
    Creating Clock using CSS + JS + PUG

---
+++ dist/index.js
@@ -0,0 +1,22 @@
+function date(){
+
+var secondHands = document.getElementById('second-hand');
+var minHands = document.getElementById('min-hand');
+var hourHands = document.getElementById('hour-hand');
+
+
+var currentDate = new Date();
+
+var seconds = currentDate.getSeconds();
+var secondsDegrees = ((seconds/60)*360) + 190;
+secondHands.style.transform = `rotate(${secondsDegrees}deg)`;
+
+var minuts = currentDate.getMinutes();
+var minDegrees = ((minuts/ 60)*360) + 90;
+minHands.style.transform = `rotate(${minDegrees}deg)`;
+
+var hours = currentDate.getHours();
+var hourDegrees = ((hours/ 60)*360) + 190;
+hourHands.style.transform = `rotate(${hourDegrees}deg)`;
+}
+setInterval(date, 1000);
